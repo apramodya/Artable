0c5482a31029cd46785df047d87554a3736cfc39
apramodya <pramodyaabeysinghe@gmail.com>
2019-03-23 19:20:20 +0800

    reuse main app home VC for admin home VC

---
+++ ArtableAdmin/AdminHomeVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Pramodya Abeysinghe on 7/3/19.
+//  Copyright © 2019 Pramodya Abeysinghe. All rights reserved.
+//
+
+import UIKit
+
+class AdminHomeVC: HomeVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.leftBarButtonItem?.isEnabled = false
+        
+        let addCategoryBtn = UIBarButtonItem(title: "Add Category", style: .plain, target: self, action: #selector(addCategory))
+        navigationItem.rightBarButtonItem = addCategoryBtn
+    }
+
+    @objc func addCategory() {
+        
+    }
+
+}
+
